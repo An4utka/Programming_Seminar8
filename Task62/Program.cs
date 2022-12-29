10859e6fcb86fda10a3b89628f04c21347959e8a
anna.v.glazunova@yandex.ru <AnnaGlazunova>
2022-12-29 19:07:10 +0300

    Task62 done

---
+++ Task62/Program.cs
@@ -0,0 +1,34 @@
+﻿// Доп.Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4. 
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int n = 4;
+int[,] matrix = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+  matrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= matrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > matrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+PrintArray(matrix);
